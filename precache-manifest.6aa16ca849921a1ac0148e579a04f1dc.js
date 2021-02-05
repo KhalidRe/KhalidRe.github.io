1555d16f4229ebc73ca77f3edaab72b1df210dcf
KhalidRe <Khaledtumah@gmail.com>
2021-02-05 01:47:47 +0100

    First Vue

---
+++ precache-manifest.6aa16ca849921a1ac0148e579a04f1dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef43ee4a2e339a189e3e",
+    "url": "/css/app.7939d646.css"
+  },
+  {
+    "revision": "b4221a662382c4c7c6bb",
+    "url": "/css/chunk-124c4eee.79d369af.css"
+  },
+  {
+    "revision": "62e766f517d9289d3496",
+    "url": "/css/chunk-3755cfb9.0f42b3b8.css"
+  },
+  {
+    "revision": "84b8940e3b9478acd3ed",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "6fd2ab14f48c9081bfdf127eddf663d5",
+    "url": "/img/Emran.6fd2ab14.gif"
+  },
+  {
+    "revision": "6cae0d57be73c6158ab531eb08fb9fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef43ee4a2e339a189e3e",
+    "url": "/js/app.04091353.js"
+  },
+  {
+    "revision": "b4221a662382c4c7c6bb",
+    "url": "/js/chunk-124c4eee.b5430228.js"
+  },
+  {
+    "revision": "62e766f517d9289d3496",
+    "url": "/js/chunk-3755cfb9.1fafbca8.js"
+  },
+  {
+    "revision": "84b8940e3b9478acd3ed",
+    "url": "/js/chunk-vendors.30a0925c.js"
+  },
+  {
+    "revision": "3c424340bd6d5c7e7a7b9b83fdd25be1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "fef6de7d0784c54cadd6eeb956104fc9",
+    "url": "/media/SkySailing.fef6de7d.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
